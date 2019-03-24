1409ce205fd930fd3e6f3ed66dd2daca236abc27
Anderson <chaosanderson1@gmail.com>
2019-03-24 14:03:23 +0800

    Update cmds.h

--- cmd_line/cmds.h
+++ cmd_line/cmds.h
@@ -21,14 +21,15 @@
 *                                          FUNCTION PROTOTYPES
 *********************************************************************************************************
 */
-uint8_t          led(uint8_t argc, char *argv[]);
-uint8_t    show_time(uint8_t argc, char *argv[]);
-uint8_t clear_window(uint8_t argc, char *argv[]);
-uint8_t    show_task(uint8_t argc, char *argv[]);
-uint8_t system_reset(uint8_t argc, char *argv[]);
-uint8_t    dev_infor(uint8_t argc, char *argv[]);
-uint8_t  set_pending(uint8_t argc, char *argv[]);
-uint8_t timer_manage(uint8_t argc, char *argv[]);
-uint8_t  task_manage(uint8_t argc, char *argv[]);
+extern uint8_t          led(uint8_t argc, char *argv[]);
+extern uint8_t    show_time(uint8_t argc, char *argv[]);
+extern uint8_t clear_window(uint8_t argc, char *argv[]);
+extern uint8_t    show_task(uint8_t argc, char *argv[]);
+extern uint8_t system_reset(uint8_t argc, char *argv[]);
+extern uint8_t    dev_infor(uint8_t argc, char *argv[]);
+extern uint8_t  set_pending(uint8_t argc, char *argv[]);
+extern uint8_t timer_manage(uint8_t argc, char *argv[]);
+extern uint8_t  task_manage(uint8_t argc, char *argv[]);
+
 #endif  /* __CMDS_H_ */
 
